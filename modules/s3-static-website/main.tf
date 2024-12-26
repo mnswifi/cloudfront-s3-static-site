518c7fc7c79ded1719f7421946184514edfc54ea
mnswifi <yusufabdulganiyu@outlook.com>
2024-12-26 21:51:37 +0100

    modified s3 public access settings

--- modules/s3-static-website/main.tf
+++ modules/s3-static-website/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "website_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket                  = aws_s3_bucket.website_bucket.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
